e03f60d869bf93adc4ed7ece121faf3191627ed8
Aldenivan Jr <aldenivan1207@gmail.com>
2022-03-03 09:19:08 -0300

    feat: Categorie model created

---
+++ app/models/categorie_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from app.configs.database import db
+from dataclasses import dataclass
+
+@dataclass
+class CategoriesModel(db.Model):
+    id: int
+    name: str
+
+    __tablename__ = "categories"
+
+    id: Column(Integer, primary_key=True)
+    name: Column(String(50), nullable=False, unique=True)
