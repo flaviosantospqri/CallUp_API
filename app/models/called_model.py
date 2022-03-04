362444ed011aea586c7f4e8a82d228cb50383dcc
Aldenivan Jr <aldenivan1207@gmail.com>
2022-03-04 11:34:20 -0300

    fix: fixing columns of the call model

--- app/models/called_model.py
+++ app/models/called_model.py
@@ -1,9 +1,12 @@
 from sqlalchemy import Column, Integer, Boolean, Date, String
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
+
 from app.configs.database import db
 from dataclasses import dataclass
 
 @dataclass
-class CalledModel(db.Model):
+class Call(db.Model):
     id: int
     description: str
     open: bool
@@ -11,7 +14,7 @@ class CalledModel(db.Model):
 
     __tablename__ = "calls"
 
-    id: Column(Integer, primary_key=True)
-    description: Column(String(225))
-    open: Column(Boolean)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    description = Column(String(225))
+    open = Column(Boolean)
     scheduling: Column(Date)
