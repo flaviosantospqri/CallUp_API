dc172c4795f8908d275251562a4ace50935ea7d6
hericfelix <84871186+hericfelix@users.noreply.github.com>
2022-03-03 22:55:02 -0300

    fix: fixed api_blueprints  imports

--- app/routes/api_blueprints.py
+++ app/routes/api_blueprints.py
@@ -1,14 +1,14 @@
-from app.routes.provider_services_blueprints import bp_provider_services
-from app.routes.sub_categories_blueprints import bp_sub_categories
-from app.routes.categories_blueprints import bp_categories
+from app.routes.provider_blueprints import bp_provider
+from app.routes.subcategory_blueprints import bp_sub_category
+from app.routes.category_blueprints import bp_category
 from app.routes.company_blueprints import bp_company
-from app.routes.called_blueprints import bp_called
+from app.routes.call_blueprints import bp_call
 from flask import Blueprint
 
 bp_api = Blueprint("bp_api", __name__, url_prefix="/api")
 
-bp_api.register_blueprint(bp_categories)
-bp_api.register_blueprint(bp_called)
-bp_api.register_blueprint(bp_sub_categories)
-bp_api.register_blueprint(bp_provider_services)
+bp_api.register_blueprint(bp_category)
+bp_api.register_blueprint(bp_call)
+bp_api.register_blueprint(bp_sub_category)
+bp_api.register_blueprint(bp_provider)
 bp_api.register_blueprint(bp_company)
