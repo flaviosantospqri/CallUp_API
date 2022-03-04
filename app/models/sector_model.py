43f5457ce5fe00eb6d9998f3920cf02e9e4a7723
Aldenivan Jr <aldenivan1207@gmail.com>
2022-03-04 11:42:33 -0300

    fix: fixing columns of the sector model

--- app/models/sector_model.py
+++ app/models/sector_model.py
@@ -1,13 +1,16 @@
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, Integer, String
+from uuid import uuid4
+
 from app.configs.database import db
 from dataclasses import dataclass
 
 @dataclass
-class SectorModel(db.Model):
+class Sector(db.Model):
     id: int
     name: str
 
     __tablename__ = "sectors"
 
-    id: Column(Integer, primary_key=True)
-    name: Column(String(225), nullable=False, unique=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = Column(String(225), nullable=False, unique=True)
