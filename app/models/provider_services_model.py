b2add0080375ddb756d184542d2e772b0ed0cf81
hericfelix <84871186+hericfelix@users.noreply.github.com>
2022-03-03 22:50:27 -0300

    fix: fixed provider model name

--- app/models/provider_services_model.py
+++ app/models/provider_services_model.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from  uuid import uuid4
 
 @dataclass
-class ProviderServicesModel(db.Model):
+class Provider(db.Model):
     id: int
     name: str
     cnpj: str
@@ -14,7 +14,7 @@ class ProviderServicesModel(db.Model):
     email: str
     password_hash: str
 
-    __tablename__ = 'provider_services'
+    __tablename__ = 'providers'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String(127), nullable=False)
