abbace7e77b2d31e9a98fbac7ee973f60b582c0a
Aldenivan Jr <aldenivan1207@gmail.com>
2022-03-04 11:37:11 -0300

    fix: fixing columns of the category model

--- app/models/categorie_model.py
+++ app/models/categorie_model.py
@@ -1,13 +1,16 @@
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, Integer, String
+from uuid import uuid4
+
 from app.configs.database import db
 from dataclasses import dataclass
 
 @dataclass
-class CategoriesModel(db.Model):
+class Category(db.Model):
     id: int
     name: str
 
     __tablename__ = "categories"
 
-    id: Column(Integer, primary_key=True)
-    name: Column(String(80), nullable=False, unique=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = Column(String(80), nullable=False, unique=True)
