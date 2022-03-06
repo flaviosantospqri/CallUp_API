4cb3f056d2a9e7b59bc57a1875d214b0f342a2a0
Flávio Santos <flavio.pasantos@outlook.com>
2022-03-06 09:08:54 -0300

    feat: create function get employees

--- app/controllers/employee_controller.py
+++ app/controllers/employee_controller.py
@@ -1,3 +1,4 @@
+from concurrent.futures.process import _ExceptionWithTraceback
 from flask import request, jsonify, session
 from http import HTTPStatus
 
@@ -6,12 +7,22 @@ from app.configs.database import db
 from sqlalchemy.orm.session import Session
 
 from sqlalchemy.exc import IntegrityError
+from app.models.company_model import Company
+from app.models.sector_model import Sector
+
+from app.models.employee_model import Employee
 
 session: Session = db.session
 
-def get_employees(id):
-    ...
-def post_employee(id):
+def get_employees():
+    try:
+        all_employees = session.query(Employee).all()
+
+        if all_employees:
+            return jsonify(all_employees), HTTPStatus.OK
+    except: 
+        return {"error": "no data found"}
+def post_employee():
     ...
 def patch_employee(id):
     ...
