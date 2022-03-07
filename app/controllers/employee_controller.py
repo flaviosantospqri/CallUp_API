0a64ea50900a24da0a86675c14050ecb6b6ae36a
Flávio Santos <flavio.pasantos@outlook.com>
2022-03-06 21:43:58 -0300

    fix: refactored structure in code

--- app/controllers/employee_controller.py
+++ app/controllers/employee_controller.py
@@ -1,8 +1,9 @@
 from concurrent.futures.process import _ExceptionWithTraceback
+from turtle import ht
 from flask import request, jsonify, session
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
+from werkzeug.exceptions import NotFound
 from app.configs.database import db
 from sqlalchemy.orm.session import Session
 
@@ -14,26 +15,37 @@ from app.models.employee_model import Employee
 
 session: Session = db.session
 
+
 @jwt_required
 def get_employees():
     try:
         all_employees = session.query(Employee).all()
         current_user = get_jwt_identity()
 
-        if current_user.type == 'employee':
-            return {"error": "access denied"}
+        if current_user.type == "employee":
+            return {"error": "access denied"}, HTTPStatus.UNAUTHORIZED
 
         if all_employees:
             return jsonify(all_employees), HTTPStatus.OK
-    except: 
-        return {"error": "no data found"}
+    except NotFound:
+        return {"error": "no data found"}, HTTPStatus.NOT_FOUND
+
+
 def post_employee():
     ...
+
+
 def patch_employee(email):
     ...
+
+
 def delete_employee(email):
     ...
+
+
 def find_employees(email):
-    ... 
+    ...
+
+
 def employee_login():
     ...
