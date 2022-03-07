464866bacb5315fc3f8fc1ca9cc40d78a69356b4
gustavolira8813 <gustavolira8813@gmail.com>
2022-03-07 11:17:52 -0300

    feat: company Route-create patch and delete routes

--- app/models/company_model.py
+++ app/models/company_model.py
@@ -15,7 +15,7 @@ class Company(db.Model):
     cnpj: String
     email: String
     address: String
-
+    
     __tablename__ = "companies"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String(100), nullable=False, unique=True)
--- app/routes/company_route.py
+++ app/routes/company_route.py
@@ -4,4 +4,9 @@ from app.controllers import company_controller
 bp = Blueprint("company", __name__, url_prefix="/company")
 
 bp.get("")(company_controller.get_companies)
-bp.get("/<int:company_id>")(company_controller.get_company_by_id)
+
+bp.post("")(company_controller.post_company)
+
+bp.patch("")(company_controller.update_company)
+
+bp.delete("")(company_controller.delete_company)
