75760c336da3b277cb02c1cc6b0ff7acf5e3982c
Flávio Santos <flavio.pasantos@outlook.com>
2022-03-08 19:53:36 -0300

    feat: refactored routes

--- app/routes/call_route.py
+++ app/routes/call_route.py
@@ -7,7 +7,6 @@ bp.get("")(call_controller.get_call)
 bp.get("/<id>")(call_controller.get_call_id)
 
 bp.post("")(call_controller.post_call)
-bp.post("")(call_controller.signin_call)
 
 bp.patch("/<id>")(call_controller.update_call)
 
