87d6e76b658a4bf968fd3b4c5ad0a47916cf5da9
gustavolira8813 <gustavolira8813@gmail.com>
2022-03-08 13:13:20 -0300

    feat:proposalRoute add routes post, patch and delete

--- app/routes/proposal_route.py
+++ app/routes/proposal_route.py
@@ -5,3 +5,9 @@ bp = Blueprint("proposal", __name__, url_prefix="/proposal")
 
 bp.get("")(proposal_controller.get_proposals)
 bp.get("")(proposal_controller.get_proposal_accepted)
+
+bp.post("")(proposal_controller.create_proposal)
+
+bp.patch("")(proposal_controller.update_proposal)
+
+bp.delete("")(proposal_controller.delete_proposal)
