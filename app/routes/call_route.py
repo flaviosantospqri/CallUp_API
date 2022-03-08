d5c2662463877a7e2c655d716a4b20e55537da8b
Flávio Santos <flavio.pasantos@outlook.com>
2022-03-08 19:49:44 -0300

    feat: create routes for controls

---
+++ app/routes/call_route.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from app.controllers import call_controller
+
+bp = Blueprint("call", __name__, url_prefix="/call")
+
+bp.get("")(call_controller.get_call)
+bp.get("/<id>")(call_controller.get_call_id)
+
+bp.post("")(call_controller.post_call)
+bp.post("")(call_controller.signin_call)
+
+bp.patch("/<id>")(call_controller.update_call)
+
+bp.delete("/<id>")(call_controller.delete_call)
