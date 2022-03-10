7c0ad950faff78675ffdf50ad17993174830be6c
Roberta Lima <robertataisaa@gmail.com>
2022-03-10 01:00:52 -0300

    Fix:Create Validates and remove checks from controllers

--- app/controllers/provider_controller.py
+++ app/controllers/provider_controller.py
@@ -6,7 +6,8 @@ from werkzeug.exceptions import NotFound, Unauthorized
 from sqlalchemy.orm.exc import UnmappedInstanceError
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from app.configs.database import db
-from app.services.provider_service import validate_cnpj, validate_email, validate_password
+from sqlalchemy.exc import IntegrityError
+import re
 
 def get_providers():
     providers = Provider.query.all()
@@ -65,29 +66,34 @@ def post_register_provider():
     session = db.session
 
     data = request.get_json()
+
+    Provider.check_fields(data)
+
+    password_regex = re.compile(r"^(((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%<^&*?])[a-zA-Z0-9!@#$%<^&*?]{8,})|([a-zA-Z]+([- .,_][a-zA-Z]+){4,}))$")
+    validate_password = re.fullmatch(password_regex, data['password'])
+    if not validate_password:
+        return {"error": "Invalid password format. A good password should be at least 8 characters long: letters, numbers and special characters. Example: Potato1@"}, HTTPStatus.BAD_REQUEST
     
     try:
-        validate_password(data["password"])
-        data["cnpj"] = validate_cnpj(data["cnpj"])
+        provider = Provider(**data)
+        session.add(provider)
+        session.commit()
 
-        print(data["cnpj"])
-
-        new_provider = Provider(**data)
-        validate_email(new_provider.email)
+    except IntegrityError:
+        return {"error": f"Provider already registred"}, HTTPStatus.CONFLICT
 
     except CnpjFormatInvalidError:
-        return {"error": "CNPJ format invalid. Format valid 00.000.000/0000-00 or 00000000000000"}
+        return {"error": "CNPJ format invalid. Format valid 00.000.000/0000-00 or 00000000000000"}, HTTPStatus.BAD_REQUEST
 
     except EmailFormatInvalidError:
-        return {"error": "Email format invalid. Format valid example@mail.com"}
+        return {"error": "Email format invalid. Format valid example@mail.com"}, HTTPStatus.BAD_REQUEST
 
     except PasswordFormatinvalidError:
-        return {"error": "Password format invalid. Use at least aA@1, examplo Batat@1"}
+        return {"error": "Password format invalid. Use at least aA@1, examplo Batat@1"}, HTTPStatus.BAD_REQUEST
 
-    session.add(new_provider)
-    session.commit()
 
-    return jsonify(new_provider), HTTPStatus.CREATED
+
+    return jsonify(provider), HTTPStatus.CREATED
 
 
 def post_login_provider():
